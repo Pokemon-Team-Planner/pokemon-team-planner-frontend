0d9c55ced42f15077b2c2ecbe0683f854f83773a
Antti Alestalo <alestalo.antti@gmail.com>
2021-12-03 18:28:20 +0200

    simplify pokemon data stored in state

--- src/reducers/pokemonReducer.js
+++ src/reducers/pokemonReducer.js
@@ -12,10 +12,15 @@ const reducer = (state = [], action) => {
 export const initializePokemon = (firstId, lastId) => {
   return async dispatch => {
     const data = await pokemonService.getRange(firstId, lastId)
+    const dataSet = data.map(pokemon => (
+      {id: pokemon.id, name: pokemon.name, types: pokemon.types, sprites: pokemon.sprites}
+    ))
+
+    console.log('dataset',dataSet)
 
     dispatch({
       type: 'INIT',
-      data
+      data: dataSet
     })
   }
 }
