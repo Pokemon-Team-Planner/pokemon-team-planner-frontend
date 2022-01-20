10f613ef6579239d7151be10d6923921441115f0
Antti Alestalo <alestalo.antti@gmail.com>
2022-01-20 20:48:02 +0200

    change base url to relative for heroku deployment

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/json'
+const baseUrl = '/json'
 
 const get = async (file) => {
   const response = await axios.get(`${baseUrl}/${file}`)
