237af42cf8bc6914dde269aa94c8acb7b2888e48
Antti Alestalo <alestalo.antti@gmail.com>
2022-04-08 17:18:58 +0300

    provide mui theme to app

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 import './index.css'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
